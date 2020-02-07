070d614ed0a6d6452bb2c1b71424bcb23e40926d
LalkaKaro4 <storis2012@yandex.ru>
2020-02-07 20:38:56 +0300

    added route switch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+
+import { history } from './constants';
+import { Spinner } from './components';
 import { NotesProvider } from './contexts';
 
 
 export const App: React.FC = () => (
-	<div className="page">
-		<div className="wrapper">
-			<NotesProvider>
-
-			</NotesProvider>
+	<Router history={history}>
+		<div className="page">
+			<div className="wrapper">
+				<NotesProvider>
+					<Suspense fallback={<Spinner />}>
+						<Switch>
 		</div>
 	</div>
+						</Switch>
+					</Suspense>
+				</NotesProvider>
+			</div>
+		</div>
+			</div>
+		</div>
+	</Router>
 );
