8c2691090c1aa08724aa6735fdf54592f5dbea85
LalkaKaro4 <storis2012@yandex.ru>
2020-02-07 21:57:09 +0300

    remove useFetch

--- src/app/contexts/NotesContext.tsx
+++ src/app/contexts/NotesContext.tsx
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { useFetch } from 'app/helpers';
 import { IList } from 'app/entities';
 import { NotesWrapper } from 'app/components';
 
 interface INotesContext {
 	isLoading: boolean;
-	data: IList | undefined;
+	data: IList;
 }
 
 const initialState = {
 	isLoading: true,
-	data: undefined,
+	data: {
+		notes: [],
+		length: 0,
+		success: false,
+		error: "",
+	},
 } as INotesContext;
 
 export const NotesContext = React.createContext<INotesContext>(initialState);
 
 export const NotesProvider: React.FC = ({ children }) => {
 
-	const { isLoading, data } = useFetch<IList>(process.env.REACT_APP_API_URL, {
-		isLoading: initialState.isLoading,
-		data: initialState.notes
-	});
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState(initialState.data);
+
+	useEffect(() => {
+
+		fetch(process.env.REACT_APP_API_URL)
+			.then<IList>(res => res.json())
+			.then(json => setData(json))
+			.then(() => setIsLoading(false))
+			.catch(() => setIsLoading(false));
+
+	}, []);
 
 	return (
 		<NotesContext.Provider value={{
 			isLoading,
-			data,
+			data
 		}}>
 			<NotesWrapper>
 				{children}
