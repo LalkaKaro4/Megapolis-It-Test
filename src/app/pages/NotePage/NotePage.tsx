67438dec15b042a161adf161906e9707e58b8229
LalkaKaro4 <storis2012@yandex.ru>
2020-02-08 01:57:24 +0300

    NotePage done

--- src/app/pages/NotePage/NotePage.tsx
+++ src/app/pages/NotePage/NotePage.tsx
@@ -1,20 +1,78 @@
 import React, { useContext, useState } from 'react';
 import { RouteComponentProps, Redirect } from 'react-router-dom';
 
-import { INoteID, INote } from 'app/entities';
+import { INoteID, INote, INoteInput } from 'app/entities';
 import { NotesContext } from 'app/contexts';
-import { PageWrapper, Button } from 'app/components';
+import { PageWrapper, Button, ButtonStyle } from 'app/components';
 
 import { ReactComponent as Trash } from "assets/svg/trash.svg";
+import { history } from 'app/constants';
 
-const Note: React.FC<INote> = ({ title }) => {
+export const ValidationTitle = (title: string) => {
+	if (!title)
+		return "Заголовок не может быть пустым";
 
-	const [isChanged, setisChanged] = useState(false);
+	return "";
+};
 
+const Note: React.FC<INote> = (note) => {
+
+	const { ChangeNote } = useContext(NotesContext);
+	const [inputs, setInputs] = useState<INoteInput>(note);
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+		const { name, value } = event.target;
+
+		setInputs(inputs => ({
+			...inputs,
+			[name]: value,
+			[name + "Error"]: ""
+		}));
+	};
+
+	const handleSumbit = () => {
+		const { title } = inputs;
+
+		if (title === note.title) {
+			return history.push("/");
+		}
+
+		const titleError = ValidationTitle(title);
+
+		setInputs(inputs => ({ ...inputs, titleError }));
+
+		if (titleError)
+			return;
+
+		ChangeNote({ ...note, title })
+			.then(() => history.push("/"));
+	};
 
 	return (
 		<div className="form">
+			<div className="form-group">
+				<label className="form-label" htmlFor="title">Краткое описание</label>
+				<input
+					className="form-input"
+					name="title"
+					type="text"
+					value={inputs.title}
+					onChange={handleInputChange}
+				/>
+				<span className="input-error" >{inputs.titleError}</span>
+			</div>
 
+			<div className="form-group">
+				<Button
+					text={inputs.title !== note.title
+						? "Сохранить"
+						: "Вернуться к списку"
+					}
+					color={ButtonStyle.BLUE}
+					onClick={handleSumbit}
+				/>
+			</div>
 		</div>
 	);
 };
@@ -35,6 +93,7 @@ const NotePage: React.FC<RouteComponentProps<INoteID>> = ({ history, match }) =>
 			header={`Задача №${idNote}`}
 			headerButton={(
 				<Button
+					color={ButtonStyle.RED}
 					Icon={Trash}
 					text="Удалить"
 					onClick={() => {
