b8d717946b7b5cd7337bbd7f683e024657e5172b
LalkaKaro4 <storis2012@yandex.ru>
2020-02-07 16:35:51 +0300

    started coding
    - css => scss
    - added vars & boot styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+// for supports IE9+
+import 'react-app-polyfill/ie9';
+
+import './styles/boot.scss';
+
+import { App } from 'app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
