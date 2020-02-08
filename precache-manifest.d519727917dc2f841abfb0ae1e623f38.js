1765f1c39b5b1167f6a242776331d9faeb8695a6
LalkaKaro4 <storis2012@yandex.ru>
2020-02-08 13:33:54 +0300

    Updates

---
+++ precache-manifest.d519727917dc2f841abfb0ae1e623f38.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af27f3923be1ceb6a41f9a46a800ea9",
+    "url": "/megapolis-it-test/index.html"
+  },
+  {
+    "revision": "c536977ab01d2fcbcaea",
+    "url": "/megapolis-it-test/static/css/3.4d67ebe4.chunk.css"
+  },
+  {
+    "revision": "1aec3dcfd396020b94dc",
+    "url": "/megapolis-it-test/static/css/4.2a01cb22.chunk.css"
+  },
+  {
+    "revision": "9e53fb3f4596c8b44e29",
+    "url": "/megapolis-it-test/static/css/main.dedaa7af.chunk.css"
+  },
+  {
+    "revision": "26721faa622cced17519",
+    "url": "/megapolis-it-test/static/js/2.3f8a808e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/megapolis-it-test/static/js/2.3f8a808e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c536977ab01d2fcbcaea",
+    "url": "/megapolis-it-test/static/js/3.8a3edd8f.chunk.js"
+  },
+  {
+    "revision": "1aec3dcfd396020b94dc",
+    "url": "/megapolis-it-test/static/js/4.4f0e4f38.chunk.js"
+  },
+  {
+    "revision": "9e53fb3f4596c8b44e29",
+    "url": "/megapolis-it-test/static/js/main.66c7926b.chunk.js"
+  },
+  {
+    "revision": "297f5c36a39ff3361feb",
+    "url": "/megapolis-it-test/static/js/runtime-main.a2b41ae4.js"
+  },
+  {
+    "revision": "a90e737d05ebfa82bf96168def807c36",
+    "url": "/megapolis-it-test/static/media/loading.a90e737d.gif"
+  },
+  {
+    "revision": "99576dbdd09665fe34a92c2994ffee8a",
+    "url": "/megapolis-it-test/static/media/pencil.99576dbd.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/megapolis-it-test/static/media/times.2c5552a1.svg"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/megapolis-it-test/static/media/trash.a3e4c2f7.svg"
+  }
+]);
